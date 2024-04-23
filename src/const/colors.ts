0feeac7f9ed335b948f7146476f1c5f1ddb336f3
quirks91 <quirks91@gmail.com>
2024-04-23 23:21:45 +0900

    Style: root 스타일 추가

--- src/const/colors.ts
+++ src/const/colors.ts
@@ -1,8 +1,16 @@
 const colors = {
   WHITE: '#fff',
+  BLACK: '#000',
   //
   PINK_500: '#bf5c79',
   PINK_700: '#c63b64',
-}
+  //
+  GRAY_200: '#e7e7e7',
+  GRAY_500: '#8e8e8e',
+  GRAY_700: '#575757',
+  //
+  RED_300: '#ffb4b4',
+  RED_500: '#ff5f5f'
+};
 
-export { colors }
+export {colors};
